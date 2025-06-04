37f3abe6efb345dd4acfff127267ada262e7ee4f
Farrux Nuranov <farruxnuranov2001@gmail.com>
2025-06-04 11:27:52 +0500

    fara

--- src/components/Header/HomeNavbar/Navbar.jsx
+++ src/components/Header/HomeNavbar/Navbar.jsx
@@ -1,22 +1,21 @@
 // src/components/Header/Navbar.jsx
-import React from 'react';
-import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom';
-import styles from './Navbar.module.scss';
-import { navLogo } from '../../../utils/getImage';
+import React from "react";
+import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
+import styles from "./Navbar.module.scss";
+import { navLogo } from "../../../utils/getImage";
 
 export default function Navbar() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
-
   const scrollToSection = (id) => {
-    if (pathname !== '/') {
-      navigate('/', { replace: true });
+    if (pathname !== "/") {
+      navigate("/", { replace: true });
       setTimeout(() => {
-        document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+        document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
       }, 50);
     } else {
-      document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+      document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -24,10 +23,14 @@ export default function Navbar() {
     <header className={styles.navbar}>
       <div className="container">
         <div className={styles.navbarInner}>
-          <Link to="#" onClick={(e) => {
-            e.preventDefault();
-            scrollToSection('navbar');
-          }} className={styles.logo}>
+          <Link
+            to="#"
+            onClick={(e) => {
+              e.preventDefault();
+              scrollToSection("navbar");
+            }}
+            className={styles.logo}
+          >
             <img src={navLogo} alt="Ameen Group" />
           </Link>
 
@@ -36,7 +39,7 @@ export default function Navbar() {
               to="#"
               onClick={(e) => {
                 e.preventDefault();
-                scrollToSection('ustozlar');
+                scrollToSection("ustozlar");
               }}
             >
               Ustozlar
@@ -45,7 +48,7 @@ export default function Navbar() {
               to="#"
               onClick={(e) => {
                 e.preventDefault();
-                scrollToSection('partnerlar');
+                scrollToSection("partnerlar");
               }}
             >
               Partnerlar
@@ -54,7 +57,7 @@ export default function Navbar() {
               to="#"
               onClick={(e) => {
                 e.preventDefault();
-                scrollToSection('toplamlar');
+                scrollToSection("toplamlar");
               }}
             >
               To`plamlar
@@ -63,7 +66,7 @@ export default function Navbar() {
               to="#"
               onClick={(e) => {
                 e.preventDefault();
-                scrollToSection('bepul-kurslar');
+                scrollToSection("bepul-kurslar");
               }}
             >
               Bepul kurslar
@@ -72,7 +75,7 @@ export default function Navbar() {
               to="#"
               onClick={(e) => {
                 e.preventDefault();
-                scrollToSection('fikrlar');
+                scrollToSection("fikrlar");
               }}
             >
               Fikrlar
@@ -80,7 +83,16 @@ export default function Navbar() {
           </nav>
 
           <div className={styles.actions}>
-            <Link to="/signup" className={styles.cta}>
+            <Link
+              onClick={() =>
+                window.gtag("event", "click_sign_up", {
+                  event_category: "button",
+                  event_label: "Записаться",
+                })
+              }
+              to=""
+              className={styles.cta}
+            >
               Ro'yxatdan o'tish
             </Link>
           </div>
