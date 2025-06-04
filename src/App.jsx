8858b870aef5758de22eb66ca9fc0ae8478ccebe
Farrux Nuranov <farruxnuranov2001@gmail.com>
2025-06-04 17:23:20 +0500

    fara

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
-import { Toaster } from 'react-hot-toast';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "react-hot-toast";
 
-import './styles/main.scss';
+import "./styles/main.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
-      <Toaster position="top-right" reverseOrder={false} />
+      <Toaster  toastOptions={{
+    success: {
+      style: {
+        fontSize: '20px',
+        padding: '16px 24px',
+        borderRadius: '10px',
+        background: '#4caf50',
+        color: '#fff',
+      },
+    },
+    error: {
+      style: {
+        fontSize: '20px',
+        padding: '16px 24px',
+        borderRadius: '10px',
+        background: '#f44336',
+        color: '#fff',
+      },
+    },
+  }} position="top-right" reverseOrder={false} />
     </BrowserRouter>
   );
 }
