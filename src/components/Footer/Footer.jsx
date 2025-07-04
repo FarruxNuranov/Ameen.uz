a950a6f0a36a1cc02dcda5bde7d435b34cd3aab7
Farrux Nuranov <farruxnuranov2001@gmail.com>
2025-07-04 12:02:19 +0500

    fara

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,200 +1,66 @@
-// src/components/Footer/Footer.jsx
 import React from "react";
 import styles from "./Footer.module.scss";
-
-import { FaMapMarkerAlt, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import {
-  instagram,
   navLogo,
+  instagram,
   telegram,
   youtube,
   facebook,
 } from "../../utils/getImage";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
   const navigate = useNavigate();
-  const { pathname } = useLocation();
-
-  const scrollToSection = (id) => {
-    if (pathname !== "/") {
-      navigate("/", { replace: true });
-      setTimeout(() => {
-        document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
-      }, 50);
-    } else {
-      document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
-    }
-  };
 
   return (
     <footer className={styles.footer}>
       <div className="container">
+        {/* Верхняя часть */}
         <div className={styles.top}>
-          {/* Бренд + форма */}
-          <div className={styles.brand}>
-            <img src={navLogo} alt="Ameen Class"  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("navbar");
-                  }} className={styles.logo} />
-            <form className={styles.subscribe}>
-              <input
-                type="tel"
-                placeholder="Telefon raqam"
-                className={styles.input}
-              />
-              <button type="submit" className={styles.button}>
-                Yuborish
-              </button>
-            </form>
-            <p className={styles.note}>
-              Iltimos telefon raqamingizni yozib qoldiring, biz siz bilan tez
-              fursatda bog‘lanamiz
-            </p>
+          <img
+            src={navLogo}
+            alt="Ameen Class"
+            className={styles.logo}
+            onClick={() => navigate("/")}
+          />
+          <div className={styles.socialIcons}>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              <img src={instagram} alt="Instagram" />
+            </a>
+            <a href="https://telegram.org" target="_blank" rel="noreferrer">
+              <img src={telegram} alt="Telegram" />
+            </a>
+            <a href="https://youtube.com" target="_blank" rel="noreferrer">
+              <img src={youtube} alt="YouTube" />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+              <img src={facebook} alt="Facebook" />
+            </a>
           </div>
+        </div>
 
-          {/* Навигация */}
-          <div className={styles.column}>
-            <h4>Navigatsiya</h4>
-            <ul>
-              <li>
-                <a
-                  href=""
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("company");
-                  }}
-                >
-                  Kompaniya haqida
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("ustozlar");
-                  }}
-                >
-                  Ustozlar
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("toplamlar");
-                  }}
-                >
-                  To‘plamlar
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("bepul-kurslar");
-                  }}
-                >
-                  Bepul kurslar
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("benefits");
-                  }}
-                >
-                  Afzalliklar
-                </a>
-              </li>
-            </ul>
+        {/* Центральная часть */}
+        <div className={styles.linkBox}>
+          <div className={styles.links}>
+            <a onClick={() => navigate("/ustozlarimiz")}>Ustozlarimiz</a>
+            <a onClick={() => navigate("/partnerlar")}>Partnerlar</a>
+            <a onClick={() => navigate("/kurslar")}>Kurslar</a>
+            <a onClick={() => navigate("/fikrlar")}>Fikrlar</a>
+            <a onClick={() => navigate("/kontaktlar")}>Kontaktlar</a>
           </div>
-
-          {/* Маълумотлар */}
-          <div className={styles.column}>
-            <h4>Ma’lumotlar</h4>
-            <ul>
-              <li>
-                <a
-                  href=""
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("partnerlar");
-                  }}
-                >
-                  Partnerlar
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("fikrlar");
-                  }}
-                >
-                  O‘quvchilarning fikrlari
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollToSection("faq");
-                  }}
-                >
-                  Tez-tez so‘raladigan savollar
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* Контакт */}
-          <div className={styles.column}>
-            <h4>Aloqa uchun</h4>
-            <ul className={styles.contactList}>
-              <li>
-                <FaMapMarkerAlt className={styles.icon} />
-                <span>
-                  International Business Center,
-                  <br />
-                  107 B Amir Temur street,Tashkent
-                </span>
-              </li>
-              <li>
-                <FaEnvelope className={styles.icon} />
-                <span>info@kozimxon.uz</span>
-              </li>
-              <li>
-                <FaPhoneAlt className={styles.icon} />
-                <span>+998 91 008 05 09</span>
-              </li>
-            </ul>
-          </div>
+          <div className={styles.links}>
+            <a onClick={() => navigate("/join")}>Ameen.uz ga qo’shilish</a>
+            <a onClick={() => navigate("/help-center")}>Help center</a>
+            <a onClick={() => navigate("/faqs")}>FAQs</a>
+          </div>  
         </div>
 
         {/* Нижняя часть */}
         <div className={styles.bottom}>
-          <p>2025 AMEEN GROUP. All rights reserved</p>
-          <div className={styles.social}>
-            <a href="https://instagram.com">
-              <img src={instagram} alt="Instagram" />
-            </a>
-            <a href="https://telegram.org">
-              <img src={telegram} alt="Telegram" />
-            </a>
-            <a href="https://youtube.com">
-              <img src={youtube} alt="YouTube" />
-            </a>
-            <a href="https://facebook.com">
-              <img src={facebook} alt="Facebook" />
-            </a>
+          <p>© 2025 Ameen uz. All rights reserved.</p>
+          <div className={styles.legal}>
+            <a onClick={() => navigate("/terms")}>Terms of Service</a>
+            <a onClick={() => navigate("/privacy")}>Privacy Policy</a>
           </div>
         </div>
       </div>
